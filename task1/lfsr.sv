8252c48d79b6c6e0ce7538034ee04e14d944d728
limenqi <enqilim2010@hotmail.com>
2025-10-31 15:39:45 +0000

    task1 including challenge

--- task1/lfsr.sv
+++ task1/lfsr.sv
@@ -2,7 +2,18 @@ module lfsr(
     input   logic       clk,
     input   logic       rst,
     input   logic       en,
-    output  logic [3:0] data_out
+    output  logic [4:1] data_out // Changed from [3:0] to [4:1] for easier indexing
 );
+
+logic [4:1] sreg;
+
+always_ff @(posedge clk or posedge rst) begin
+    if (rst)
+        sreg <= 4'b0001;
+    else if (en)
+        sreg <= {sreg[3:1], sreg[4] ^ sreg[3]};
+end
+
+assign data_out = sreg;
 
 endmodule
--- task1/lfsr_7.sv
+++ task1/lfsr_7.sv
@@ -2,7 +2,18 @@ module lfsr_7 (
     input   logic       clk,
     input   logic       rst,
     input   logic       en,
-    output  logic [6:0] data_out
+    output  logic [7:1] data_out
 );
+
+logic [7:1] sreg;
+
+always_ff @(posedge clk or posedge rst) begin
+    if (rst)
+        sreg <= 7'b0000001;
+    else if (en)
+        sreg <= {sreg[6:1], sreg[3] ^ sreg[7]};
+end
+
+assign data_out = sreg;
 
 endmodule
